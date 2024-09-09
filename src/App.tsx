e912ba3ca5ed845e1e1d21d0e02020fbff5b8f69
Tomas Pesek <pesekt@gmail.com>
2024-09-09 11:54:37 +0200

    fetching games from public API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const apiClient = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: import.meta.env.VITE_API_KEY,
+  },
+});
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface GamesResponse {
+  count: number;
+  results: Game[];
+}
 
 function App() {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClient
+      .get<GamesResponse>("/games")
+      .then((response) => setGames(response.data.results))
+      .catch((error) => setError(error.message));
+  }, []);
+
   return (
     <Grid
       templateAreas={{
