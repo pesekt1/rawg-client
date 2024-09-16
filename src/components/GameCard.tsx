594334e25ae83127374f01b68c2ba033bf842fd1
Tomas Pesek <pesekt@gmail.com>
2024-09-16 09:17:41 +0200

    added parent platforms to the game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 
 interface Props {
@@ -11,6 +11,9 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        {game.parent_platforms.map((pp) => (
+          <Text key={pp.platform.id}>{pp.platform.name}</Text>
+        ))}
       </CardBody>
     </Card>
   );
